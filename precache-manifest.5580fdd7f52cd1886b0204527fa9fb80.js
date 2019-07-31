f89e0802bb1e9079aa394eafff8664666c905350
KimChangyeon <doli1573@kaist.ac.kr>
2019-07-31 22:11:15 +0900

    Updates

---
+++ precache-manifest.5580fdd7f52cd1886b0204527fa9fb80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07305d546a891fe80702b4e36bea5110",
+    "url": "/twitch/index.html"
+  },
+  {
+    "revision": "6b17463634a0cc3e95c0",
+    "url": "/twitch/static/css/main.85e8c171.chunk.css"
+  },
+  {
+    "revision": "2583f61837f9ed3d0d1f",
+    "url": "/twitch/static/js/2.d37b1182.chunk.js"
+  },
+  {
+    "revision": "6b17463634a0cc3e95c0",
+    "url": "/twitch/static/js/main.3a4ba2e9.chunk.js"
+  },
+  {
+    "revision": "f7a549816f0cf02544f5",
+    "url": "/twitch/static/js/runtime~main.dc4b977b.js"
+  }
+]);
